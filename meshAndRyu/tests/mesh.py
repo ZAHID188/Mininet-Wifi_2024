b3b230236e0b6636ba76c593179139e373d6537a
ZAHID188 <zahidpranta5@gmail.com>
2024-05-14 13:47:21 +0800

    mesh with ryu - test add new AP and station

--- meshAndRyu/tests/mesh.py
+++ meshAndRyu/tests/mesh.py
@@ -18,10 +18,13 @@ def topology():
     net = Mininet_wifi(controller=RemoteController, link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
-    sta1 = net.addStation('sta1', mac='00:00:00:00:00:11', position='1,1,0')
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:12', position='31,11,0')
-    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1', position='10,10,0')
-    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2', position='30,10,0')
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:11', position='20,51,0')
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:12', position='51,51,0')
+    sta3 = net.addStation('sta3', mac='00:00:00:00:00:13', position='81,51,0')
+
+    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1', position='30,60,0')
+    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2', position='50,60,0')
+    ap3 = net.addAccessPoint('ap3', wlans=2, ssid='ssid2', position='70,60,0')
     c0 = net.addController('c1', controller=RemoteController, ip='127.0.0.1', port=6653)
 
     info("*** Configuring nodes\n")
@@ -30,14 +33,20 @@ def topology():
     info("*** Associating Stations\n")
     net.addLink(sta1, ap1)
     net.addLink(sta2, ap2)
+    net.addLink(sta3, ap3)
     net.addLink(ap1, intf='ap1-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
     net.addLink(ap2, intf='ap2-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
+    net.addLink(ap3, intf='ap3-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
+    net.plotGraph(max_x=160, max_y=160) 
 
     info("*** Starting network\n")
     net.build()
     c0.start()
     ap1.start([c0])
     ap2.start([c0])
+    ap3.start([c0])
+    
+     
 
     info("*** Running CLI\n")
     CLI(net)
